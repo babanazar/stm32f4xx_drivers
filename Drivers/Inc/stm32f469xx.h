a576ceca050e5c56299dfc6f29cfa4a139bd58e5
Babanazar <g.babanazar@gmail.com>
2021-03-16 20:49:06 +0300

    drivers completed for now

--- Drivers/Inc/stm32f469xx.h
+++ Drivers/Inc/stm32f469xx.h
@@ -8,6 +8,10 @@
 #ifndef INC_STM32F469XX_H_
 #define INC_STM32F469XX_H_
 
+#include <stdint.h>
+
+#define __IO					volatile
+
 /*
  * Base addresses of Flash and SRAM memories
  */
@@ -42,6 +46,8 @@
 #define GPIOJ_BASEADDR			(AHB1PERIPH_BASEADDR + 0x2400)
 #define GPIOK_BASEADDR			(AHB1PERIPH_BASEADDR + 0x2800)
 
+#define RCC_BASEADDR			(AHB1PERIPH_BASEADDR + 0x3800)
+
 /*
  * Base addresses of peripherals which are hanging on APB1 bus.
  */
@@ -66,7 +72,163 @@
 #define SPI1_BASEADDR			(APB2PERIPH_BASEADDR + 0x3000)
 #define SYSCFG_BASEADDR			(APB2PERIPH_BASEADDR + 0x3800)
 #define USART1_BASEADDR			(APB2PERIPH_BASEADDR + 0x1000)
-#define USART2_BASEADDR			(APB2PERIPH_BASEADDR + 0x1400)
+#define USART6_BASEADDR			(APB2PERIPH_BASEADDR + 0x1400)
 
+
+/******************* Peripheral Register Definition Structures *******************/
+
+typedef struct
+{
+	__IO uint32_t MODER;						/* GPIO port mode register. Address offset:0x00 */
+	__IO uint32_t OTYPER;					/* GPIO port output type register. Address offset:0x04 */
+	__IO uint32_t OSPEEDR;					/* GPIO port output speed register. Address offset: 0x08 */
+	__IO uint32_t PUPDR;						/* GPIO port pull-up/pull-down register. Address offset:0x0C */
+	__IO uint32_t IDR;						/* GPIO port input data register. Address offset:0x10 */
+	__IO uint32_t ODR;						/* GPIO port output data register. Address offset:0x14 */
+	__IO uint32_t BSRR;						/* GPIO port mode register.	Address offset:0x00 */
+	__IO uint32_t LCKR;						/* GPIO port mode register.	Address offset:0x00 */
+	__IO uint32_t AFR[2];					/* GPIO port mode register.	Address offset:0x00 */
+}GPIO_RegDef_t;
+
+typedef struct
+{
+	__IO uint32_t CR;
+	__IO uint32_t PLLCFGR;
+	__IO uint32_t CFGR;
+	__IO uint32_t CIR;
+	__IO uint32_t AHB1RSTR;
+	__IO uint32_t AHB2RSTR;
+	__IO uint32_t AHB3RSTR;
+	uint32_t RESERVED;
+	__IO uint32_t APB1RSTR;
+	__IO uint32_t APB2RSTR;
+	uint32_t RESERVED1[2];
+	__IO uint32_t AHB1ENR;
+	__IO uint32_t AHB2ENR;
+	__IO uint32_t AHB3ENR;
+	uint32_t RESERVED2;
+	__IO uint32_t APB1ENR;
+	__IO uint32_t APB2ENR;
+	uint32_t RESERVED3[2];
+	__IO uint32_t AHB1LPENR;
+	__IO uint32_t AHB2LPENR;
+	__IO uint32_t AHB3LPENR;
+	uint32_t RESERVED4;
+	__IO uint32_t APB1LPENR;
+	__IO uint32_t APB2LPENR;
+	uint32_t RESERVED5[2];
+	__IO uint32_t BDSR;
+	__IO uint32_t CSR;
+	uint32_t RESERVED6[2];
+	__IO uint32_t SSCGR;
+	__IO uint32_t PLLI2SSCGR;
+	__IO uint32_t PLLSAICFGR;
+	__IO uint32_t DCKCFGR;
+}RCC_RegDef_t;
+
+
+/*
+ * Peripheral definitions (Peripheral base addresses typecasted to xxx_RegDef_t )
+ */
+#define GPIOA			((GPIO_RegDef_t*)GPIOA_BASEADDR)
+#define GPIOB			((GPIO_RegDef_t*)GPIOB_BASEADDR)
+#define GPIOC			((GPIO_RegDef_t*)GPIOC_BASEADDR)
+#define GPIOD			((GPIO_RegDef_t*)GPIOD_BASEADDR)
+#define GPIOE			((GPIO_RegDef_t*)GPIOE_BASEADDR)
+#define GPIOF			((GPIO_RegDef_t*)GPIOF_BASEADDR)
+#define GPIOG			((GPIO_RegDef_t*)GPIOG_BASEADDR)
+#define GPIOH			((GPIO_RegDef_t*)GPIOH_BASEADDR)
+#define GPIOI			((GPIO_RegDef_t*)GPIOI_BASEADDR)
+#define GPIOJ			((GPIO_RegDef_t*)GPIOJ_BASEADDR)
+#define GPIOK			((GPIO_RegDef_t*)GPIOK_BASEADDR)
+
+
+#define RCC				((RCC_RegDef_t*)RCC_BASEADDR)
+
+
+/*
+ * Clock Enable Macros for GPIOx peripherals
+ */
+#define GPIOA_PCLK_EN()		(RCC->AHB1ENR |= (1 << 0))
+#define GPIOB_PCLK_EN()		(RCC->AHB1ENR |= (1 << 1))
+#define GPIOC_PCLK_EN()		(RCC->AHB1ENR |= (1 << 2))
+#define GPIOD_PCLK_EN()		(RCC->AHB1ENR |= (1 << 3))
+#define GPIOE_PCLK_EN()		(RCC->AHB1ENR |= (1 << 4))
+#define GPIOF_PCLK_EN()		(RCC->AHB1ENR |= (1 << 5))
+#define GPIOG_PCLK_EN()		(RCC->AHB1ENR |= (1 << 6))
+#define GPIOH_PCLK_EN()		(RCC->AHB1ENR |= (1 << 7))
+#define GPIOI_PCLK_EN()		(RCC->AHB1ENR |= (1 << 8))
+#define GPIOJ_PCLK_EN()		(RCC->AHB1ENR |= (1 << 9))
+#define GPIOK_PCLK_EN()		(RCC->AHB1ENR |= (1 << 10))
+
+/*
+ * Clock Enable Macros for I2Cx peripherals
+ */
+#define I2C1_PCLK_EN()		(RCC->APB1ENR |= (1<<21))
+#define I2C2_PCLK_EN()		(RCC->APB1ENR |= (1<<22))
+#define I2C3_PCLK_EN()		(RCC->APB1ENR |= (1<<23))
+
+
+/*
+ * Clock Enable Macros for SPIx peripherals
+ */
+#define SPI1_PCLK_EN()		(RCC->APB2ENR |= (1<<12))
+#define SPI2_PCLK_EN()		(RCC->APB1ENR |= (1<<14))
+#define SPI3_PCLK_EN()		(RCC->APB1ENR |= (1<<15))
+
+
+/*
+ * Clock Disable Macros for GPIOx peripherals
+ */
+#define GPIOA_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 0))
+#define GPIOB_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 1))
+#define GPIOC_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 2))
+#define GPIOD_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 3))
+#define GPIOE_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 4))
+#define GPIOF_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 5))
+#define GPIOG_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 6))
+#define GPIOH_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 7))
+#define GPIOI_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 8))
+#define GPIOJ_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 9))
+#define GPIOK_PCLK_DI()		(RCC->AHB1ENR &= ~(1 << 10))
+
+/*
+ * Clock Disable Macros for SPIx peripherals
+ */
+
+
+/*
+ * Clock Disable Macros for USARTx peripherals
+ */
+
+/*
+ * Clock Disable Macros for SYSCFG peripherals
+ */
+
+/*
+ * Macros to reset GPIOx peripherals
+ */
+#define GPIOA_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 0));	(RCC->AHB1RSTR &= ~(1 << 0));}while(0)
+#define GPIOB_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 1));	(RCC->AHB1RSTR &= ~(1 << 1));}while(0)
+#define GPIOC_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 2));	(RCC->AHB1RSTR &= ~(1 << 2));}while(0)
+#define GPIOD_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 3));	(RCC->AHB1RSTR &= ~(1 << 3));}while(0)
+#define GPIOE_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 4));	(RCC->AHB1RSTR &= ~(1 << 4));}while(0)
+#define GPIOF_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 5));	(RCC->AHB1RSTR &= ~(1 << 5));}while(0)
+#define GPIOG_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 6));	(RCC->AHB1RSTR &= ~(1 << 6));}while(0)
+#define GPIOH_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 7));	(RCC->AHB1RSTR &= ~(1 << 7));}while(0)
+#define GPIOI_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 8));	(RCC->AHB1RSTR &= ~(1 << 8));}while(0)
+#define GPIOJ_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 9));	(RCC->AHB1RSTR &= ~(1 << 9));}while(0)
+#define GPIOK_REG_RESET()		do { (RCC->AHB1RSTR |= (1 << 10));	(RCC->AHB1RSTR &= ~(1 << 10));}while(0)
+
+
+/*
+ * Some generic macros
+ */
+#define ENABLE 			1
+#define DISABLE 		0
+#define SET				1
+#define RESET			0
+#define GPIO_PIN_SET	SET
+#define GPIO_PIN_RESET	RESET
 
 #endif /* INC_STM32F469XX_H_ */
---
+++ Drivers/Src/stm32f469xx_gpio_driver.c
@@ -0,0 +1,264 @@
+/*
+ * stm32469xx_gpio_driver.c
+ *
+ *  Created on: Mar 16, 2021
+ *      Author: Babanazar
+ */
+
+#include "stm32f469xx_gpio_driver.h"
+
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t enOrDi)
+{
+	if(enOrDi == ENABLE)
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOB)
+		{
+			GPIOB_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOC)
+		{
+			GPIOC_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOD)
+		{
+			GPIOD_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOE)
+		{
+			GPIOE_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOF)
+		{
+			GPIOF_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOG)
+		{
+			GPIOG_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOH)
+		{
+			GPIOH_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOI)
+		{
+			GPIOI_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOJ)
+		{
+			GPIOJ_PCLK_EN();
+		}
+		else if(pGPIOx == GPIOK)
+		{
+			GPIOK_PCLK_EN();
+		}
+
+	}
+	else
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOB)
+		{
+			GPIOB_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOC)
+		{
+			GPIOC_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOD)
+		{
+			GPIOD_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOE)
+		{
+			GPIOE_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOF)
+		{
+			GPIOF_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOG)
+		{
+			GPIOG_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOH)
+		{
+			GPIOH_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOI)
+		{
+			GPIOI_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOJ)
+		{
+			GPIOJ_PCLK_DI();
+		}
+		else if(pGPIOx == GPIOK)
+		{
+			GPIOK_PCLK_DI();
+		}
+	}
+}
+
+
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+	uint32_t temp = 0;
+
+	//	1. Configure the mode of gpio pin
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		// non-interrupt mode
+		temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+		pGPIOHandle->pGPIOx->MODER &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);  // clearing
+		pGPIOHandle->pGPIOx->MODER |= temp;  // setting
+	}
+	else
+	{
+		// interrupt mode
+	}
+	temp = 0;
+
+	//	2. Configure the speed
+
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);  // clearing
+	pGPIOHandle->pGPIOx->OSPEEDR |= temp;
+
+	temp = 0;
+
+	//	3. Configure the pupd settings
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);  // clearing
+	pGPIOHandle->pGPIOx->PUPDR |= temp;
+
+	temp = 0;
+
+	//	4. Configure the optype
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);  // clearing
+	pGPIOHandle->pGPIOx->OTYPER |= temp;
+
+	temp = 0;
+
+	//	5. Configure the alt functionality
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN)
+	{
+		// configure the alt function registers
+		uint32_t temp1, temp2;
+
+		temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 8;
+		temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 8;
+
+		pGPIOHandle->pGPIOx->AFR[temp1] &= ~(0xF << (4*temp2));
+		pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4*temp2));
+	}
+}
+
+
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
+{
+	if(pGPIOx == GPIOA)
+	{
+		GPIOA_REG_RESET();
+	}
+	else if(pGPIOx == GPIOB)
+	{
+		GPIOB_REG_RESET();
+	}
+	else if(pGPIOx == GPIOC)
+	{
+		GPIOC_REG_RESET();
+	}
+	else if(pGPIOx == GPIOD)
+	{
+		GPIOD_REG_RESET();
+	}
+	else if(pGPIOx == GPIOE)
+	{
+		GPIOE_REG_RESET();
+	}
+	else if(pGPIOx == GPIOF)
+	{
+		GPIOF_REG_RESET();
+	}
+	else if(pGPIOx == GPIOG)
+	{
+		GPIOG_REG_RESET();
+	}
+	else if(pGPIOx == GPIOH)
+	{
+		GPIOH_REG_RESET();
+	}
+	else if(pGPIOx == GPIOI)
+	{
+		GPIOI_REG_RESET();
+	}
+	else if(pGPIOx == GPIOJ)
+	{
+		GPIOJ_REG_RESET();
+	}
+	else if(pGPIOx == GPIOK)
+	{
+		GPIOK_REG_RESET();
+	}
+}
+
+
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber)
+{
+	uint8_t value;
+
+	value = (uint8_t)((pGPIOx->IDR >> pinNumber) & 0x00000001);
+
+	return value;
+}
+
+
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
+{
+	uint16_t value;
+
+	value = (uint16_t)pGPIOx->IDR;
+
+	return value;
+}
+
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber, uint8_t value)
+{
+
+	if(value == GPIO_PIN_SET)
+	{
+		pGPIOx->ODR |= (1 << pinNumber);
+	}
+	else
+	{
+		pGPIOx->ODR &= ~(1 << pinNumber);
+	}
+}
+
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t value)
+{
+	pGPIOx->ODR = value;
+}
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber)
+{
+	pGPIOx->ODR ^= (1 << pinNumber);
+}
+
+
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t enOrDi)
+{
+
+}
+
+void GPIO_IRQHandling(uint8_t pinNumber)
+{
+
+}
